9b7bcab3f8adcc76f81359a94888a2f78c017adb
I306445 <allen.yin@sap.com>
2019-02-28 17:18:25 +0800

    change from test stage to release stage

--- controller/globals.php
+++ controller/globals.php
@@ -6,8 +6,8 @@
  * Time: 08:53
  */
 
-define("WEBTITLE", "(测试中)徐汇区逸夫小学俱乐部报名系统");
-define("ADMINWEBTITLE", "(测试中)报名系统后台");
+define("WEBTITLE", "徐汇区逸夫小学俱乐部报名系统");
+define("ADMINWEBTITLE", "报名系统后台");
 define("HOMENAME", "徐汇区逸夫小学");
 define("ADMINPAGENAME", "后台");
 define("USERPAGENAME", "主页");
